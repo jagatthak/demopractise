1aa409c218b567ee4b189afc609ef82c0e360826
jagatthak <jagatthakkar25@gmail.com>
2022-05-01 11:10:13 +0100

    Timetable for 2

---
+++ src/Timetable.java
@@ -0,0 +1,11 @@
+public class Timetable {
+
+    public static void main(String[] args) {
+        int i=1;
+       int a=2;
+        while(i<=10){
+            System.out.println(a + " * "+ i + " = "+(i*a));
+            i++;
+        }
+    }
+}
